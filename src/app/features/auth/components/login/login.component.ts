b2a28e40878f37e57a08ed0ef9908f0689e8e8c0
AlIDJAT <alidjatou1@gmail.com>
2025-03-13 07:28:08 +0100

    GREEN - Implement LoginComponent to pass tests

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
 
+  constructor(private authService: AuthService) {}
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.authService.login(email!, password!).subscribe({
+        next: (res) => console.log('Login successful', res),
+        error: (err) => console.error('Login failed', err),
+      });
+    }
+  }
 }
