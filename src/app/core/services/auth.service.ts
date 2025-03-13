b0c2f2978902d76e4fea05aff7c56f9aa51e2fa9
AlIDJAT <alidjatou1@gmail.com>
2025-03-13 07:01:20 +0100

    REFACTOR - Move API URL to environment configuration

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
+import { environment } from '../../../environements/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8080/auth/login'; // URL du backend
+  private apiUrl = `${environment.apiBaseUrl}/auth/login`; // URL du backend
 
   constructor(private http: HttpClient) {}
 
