6be9a613e0d677ea423f61e8643cc2dfef7ac35b
AlIDJAT <alidjatou1@gmail.com>
2025-03-13 08:22:47 +0100

    RED: Generate Dashboard module and component

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,17 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from '../../../environements/environment';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private authUrl = `${environment.apiBaseUrl}/auth`;
+  private tokenKey = 'auth_token';
 
   constructor(private http: HttpClient) {}
 
   login(email: string, password: string): Observable<{ token: string }> {
-    return this.http.post<{ token: string }>(`${this.authUrl}/login`, { email, password });
+    return this.http.post<{ token: string }>(`${environment.apiBaseUrl}/auth/login`, { email, password }).pipe(
+      tap(response => {
+        localStorage.setItem(this.tokenKey, response.token);
+      })
+    );
+  }
 
+  logout(): void {
+    localStorage.removeItem(this.tokenKey); 
+  }
+
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem(this.tokenKey); 
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey); 
   }
 }
