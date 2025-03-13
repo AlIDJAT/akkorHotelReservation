a69445affe71f8c7bc436fa99d809d04192a56d4
AlIDJAT <alidjatou1@gmail.com>
2025-03-13 07:27:25 +0100

    RED - Add failing tests for LoginComponent

--- src/app/features/auth/components/login/login.component.spec.ts
+++ src/app/features/auth/components/login/login.component.spec.ts
@@ -1,23 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [LoginComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+      imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule],
+      providers: [AuthService]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a form with email and password fields', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('input[type="email"]')).toBeTruthy();
+    expect(compiled.querySelector('input[type="password"]')).toBeTruthy();
+  });
+
+  it('should call login method when clicking submit', () => {
+    spyOn(component, 'onLogin');
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    expect(component.onLogin).toHaveBeenCalled();
   });
 });
